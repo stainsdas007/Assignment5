db0c3f3f43beac0ed6b5299ed4b139ced9c24812
stainsdas007 <stainsdas007@gmail.com>
2025-01-27 11:42:25 +0530

    Add files via upload

---
+++ Assignment5-5E.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment5_5E
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a method to check whether a number is prime or not. 
+
+
+        }
+
+        static void PrimeNumber(int number)
+        {
+            if (number < 2)
+            {
+                // ($"{number} is not a prime number.");
+            }
+
+        }
+    }
+}
